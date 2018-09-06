4c5c9762d609a1daaca95aa4c65d73145da23600
Somroy1993 <39045323+Somroy1993@users.noreply.github.com>
2018-09-06 22:01:25 +0530

    Update program2find_bob.py

--- program2find_bob.py
+++ program2find_bob.py
@@ -1,6 +1,6 @@
-c=0
+c=0 #count
 s=input("input the string")
 for i in range(len(s)):
     if s[i]=='b' and s[i+1]=='o' and s[i+2]=='b':
         c+=1
-print(c)
+print(c) #returns number time bob is repeated
